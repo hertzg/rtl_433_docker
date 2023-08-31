2881c34dcd8a64645c688e4626c7505f1408eec4
Giorgi Kotchlamazashvili <georgedot@gmail.com>
2023-08-31 21:49:50 +0400

    feat: add documented arches to alpine

--- src/alpine.config.ts
+++ src/alpine.config.ts
@@ -87,10 +87,13 @@ export const createAlpineBuildTasks = (
           alpineVersion: alpineVersion,
         },
         platforms: [
+          "linux/386",
           "linux/amd64",
           "linux/arm/v6",
           "linux/arm/v7",
           "linux/arm64/v8",
+          "linux/ppc64le",
+          "linux/s390x",
         ],
         cacheFrom: `type=gha,scope=alpine-${alpineVersion}-${gitRef}`,
         cacheTo: `type=gha,scope=alpine-${alpineVersion}-${gitRef}`,
